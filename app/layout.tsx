0a41327bab8b8decd18ee6e128bef7cd490f9fec
ych <943696882@qq.com>
2026-03-26 15:30:46 +0800

    fix icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,9 @@ export const metadata: Metadata = {
     "google-adsense-account": "ca-pub-8738419268805262",
   },
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/favicon.png",
+    apple: "/favicon.png",
+    shortcut: "/favicon.png",
   },
 }
 
